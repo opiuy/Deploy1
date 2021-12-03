d2617438dc6f926dacd576670b1276cd25ad053a
opiuy <murtipiyush1@gmail.com>
2021-12-03 16:51:47 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import { Home } from './Home';
+
+
+const  countAge = () => {
+}
+
+function App() {
+  return (
+ <>
+
+<h1>XYZ</h1>
+<Home />
+ </>
+   );
+}
+
+export default App;
---
+++ src/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Home.css'
+export const Home = () => {
+    return (
+        <div className='container'>
+            <h1>Welcome to React</h1>
+        </div>
+    )
+}
